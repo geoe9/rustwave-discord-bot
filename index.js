6c246158e48af588a1a5adee45e5dbfc8da298af
George <geo.rog09@gmail.com>
2024-10-02 20:50:08 +0100

    Call inviteManager once client is ready

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const { Client, Events, GatewayIntentBits } = require('discord.js');
 const config = require('./config.json');
+const inviteManager = require('./src/inviteManager.js')
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildInvites, GatewayIntentBits.GuildMembers] });
 
 client.once(Events.ClientReady, readyClient => {
-	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+	console.log(`Successfully logged in to Discord as ${readyClient.user.tag}`);
+    inviteManager(client)
 });
 
 client.login(config.discord.token);
