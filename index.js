182e1a58596ce3c1c4c33b37afb7773ffee40a0c
George <geo.rog09@gmail.com>
2024-10-03 12:42:28 +0100

    Add event listeners

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 const { Client, Events, GatewayIntentBits } = require('discord.js');
 const config = require('./config.json');
 const inviteManager = require('./src/inviteManager.js')
+//const userManager = require('./src/userManager.js')
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildInvites, GatewayIntentBits.GuildMembers] });
 
-client.once(Events.ClientReady, readyClient => {
+client.once(Events.ClientReady, async readyClient => {
 	console.log(`Successfully logged in to Discord as ${readyClient.user.tag}`);
-    inviteManager(client)
+    const guild = await client.guilds.fetch(config.discord.serverid);
+    inviteManager.updateInviteTable(guild)
+});
+
+client.on(Events.GuildMemberAdd, async member => {
+    const guild = await client.guilds.fetch(config.discord.serverid);
+    inviteManager.processNewMember(guild, member);    
+});
+
+client.on(Events.InviteCreate, async invite => {
+    inviteManager.processNewInvite(invite)
 });
 
 client.login(config.discord.token);
